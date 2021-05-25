71f3e09ee45033396cb4d88a6bdf3e1bf53546e8
mateuskuritza <mateuskuritza@gmail.com>
2021-05-25 19:04:45 -0300

    responsive

--- src/components/general/Avatar/index.js
+++ src/components/general/Avatar/index.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 
-export default function Avatar(props){
+export default function Avatar(){
 
-    // width em px
-    const { width } = props;
-    
     return(
-        <Logo src="https://cozinhatecnica.com/wp-content/uploads/2019/11/massa-de-pastel.jpg" width={width}/>
+        <Logo src="https://cozinhatecnica.com/wp-content/uploads/2019/11/massa-de-pastel.jpg"/>
     );
 }
 
 const Logo = styled.img`
     border-radius: 26.5px;
-    width: ${props => props.width};
-    height: ${props => props.width};
     object-fit: cover;
+    width: 40px;
+    height: 40px;
+
+    @media (min-width: 750px){
+        width: 50px;
+        height: 50px;
+    }
 `;
