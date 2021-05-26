9b582af07625c45e83c9294befcc9cf113b09826
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-05-26 18:31:21 -0300

    Posts refresh

--- src/components/Timeline/MyLikes.js
+++ src/components/Timeline/MyLikes.js
@@ -10,14 +10,19 @@ export default function MyLikes (props) {
     const {token} = useContext(UserContext);
 
     const history = useHistory();
+    const [refresh, setRefresh] = React.useState([]);
+
+    function refreshPosts () {
+        setRefresh([...refresh]);
+    }
+
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
 
     useEffect(() => {
-        const config = {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        }
-
         const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/liked,`, config)
 
         request.then((response) => {
@@ -26,9 +31,9 @@ export default function MyLikes (props) {
             alert("Faça login novamente!");
             history.push("/");
         })
-    },[]);
+    },[refresh]);
 
     return (
-        <TimelineLayout posts={posts} title={"my likes"}/>
+        <TimelineLayout posts={posts} title={"my likes"} refreshPosts={refreshPosts}/>
     );
 }
--- src/components/Timeline/MyPosts.js
+++ src/components/Timeline/MyPosts.js
@@ -10,14 +10,19 @@ export default function MyPosts() {
     const {user, token} = useContext(UserContext);
 
     const history = useHistory();
+    const [refresh, setRefresh] = React.useState([]);
+
+    function refreshPosts () {
+        setRefresh([...refresh]);
+    }
+
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
 
     useEffect(() => {
-        const config = {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        }
-
         const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${user.id}/posts,`, config)
 
         request.then((response) => {
@@ -26,9 +31,9 @@ export default function MyPosts() {
             alert("Faça login novamente!");
             history.push("/");
         })
-    },[]);
+    },[refresh]);
 
     return (
-        <TimelineLayout posts={posts} title={"my posts"}/>
+        <TimelineLayout posts={posts} title={"my posts"} refreshPosts={refreshPosts}/>
     );
 }
