1add4ee8369511b03ff8e1ac55522ed90c1b4527
mateuskuritza <mateuskuritza@gmail.com>
2021-06-01 15:21:27 -0300

    console.log remove

--- src/components/general/Navbar/SearchUsers.js
+++ src/components/general/Navbar/SearchUsers.js
@@ -30,7 +30,6 @@ export default function SearchUsers() {
             const ordenedUsers = following.concat(unFollowing);
 
             setUsers(ordenedUsers);
-            console.log(ordenedUsers);
         });
 
     }, [inputValue])
