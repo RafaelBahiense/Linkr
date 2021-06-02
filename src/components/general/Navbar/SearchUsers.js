49da84a49ff4d84cd2ed546a76222f2262b8b081
mateuskuritza <mateuskuritza@gmail.com>
2021-06-02 18:11:58 -0300

    input border bug fix

--- src/components/general/Navbar/SearchUsers.js
+++ src/components/general/Navbar/SearchUsers.js
@@ -9,7 +9,8 @@ export default function SearchUsers(props) {
   const [showingSuggestions, setShowingSuggestions] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [users, setUsers] = useState([]);
-
+  const [onFocusInput, setOnFocusInput] = useState(false);
+  
   const { token } = useContext(UserContext);
 
   const config = {
@@ -38,14 +39,20 @@ export default function SearchUsers(props) {
   }, [inputValue]);
 
   return (
-    <SearchContainer width={props.width} valueSize={inputValue.length}>
+    <SearchContainer width={props.width} valueSize={inputValue.length} onFocusInput={onFocusInput}>
       <DebounceInput
         minLength={3}
         debounceTimeout={300}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        onBlur={() => setTimeout(() => setShowingSuggestions(false), 150)}
-        onFocus={() => setShowingSuggestions(true)}
+        onBlur={() => {
+          setTimeout(() => setShowingSuggestions(false), 150);
+          setOnFocusInput(false);
+        }}
+        onFocus={() => {
+          setShowingSuggestions(true);
+          setOnFocusInput(true);
+        }}
         placeholder="Search for people and friends"
       ></DebounceInput>
       {showingSuggestions && <Suggestions users={users} />}
@@ -59,7 +66,7 @@ const SearchContainer = styled.div`
   z-index: 11;
   margin: 0 auto;
   background: #e7e7e7;
-  border-radius: ${(props) => (props.valueSize >= 3 ? "8px 8px 0 0" : "8px")};
+  border-radius: ${(props) => (props.valueSize >= 3 && props.onFocusInput ? "8px 8px 0 0" : "8px")};
   margin-top: ${(props) => (props.width > 850 ? "0" : "90px")};
   margin-bottom: ${(props) => (props.width > 850 ? "0" : "-100px")};
   width: ${(props) => (props.width > 850 ? "60%" : "70%")};
